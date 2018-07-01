343fface3cfcfaedd3447ad8f613d976f5525b3a
sarmasida <39568866+sarmasida@users.noreply.github.com>
2018-07-01 23:58:36 +0200

    fix
    glupi folderi

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,29 +4,28 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_mapping(
-		SECRET_KEY='dev',
-		DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-	)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-	if test_config is None:
-		app.config.from_pyfile('config.py', silent=True)
-	else:
-		app.config.from_mapping(test_config)
-	try:
-		os.makedirs(app.instance_path)
-	except OSError:
-		pass
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-	@app.route('/hello')
-	def hello():
-		return 'Hello, World!'
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
 
-	from . import db, auth
-	db.init_app(app)
-	app.register_blueprint(auth.bp)
+    from . import db
+    db.init_app(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
 
-	return app
-
-
+    return app
